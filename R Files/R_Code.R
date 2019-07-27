f81e067f5824933991bec41c00029bd36c85eed3
Jeffrey Jose <49552078+Jeffreyjose29@users.noreply.github.com>
2019-07-27 14:34:51 +1200

    Added a few comments and tried printing the position using a vector

--- R Files/R_Code.R
+++ R Files/R_Code.R
@@ -4,29 +4,30 @@
 generateWalkFunc <- function(numSteps, stepRange){
   #define the initial starting point
   initial.value <- 0
+  #set the current point before the first iteration to the starting point
+  #the randomWalk always starts at 0 (initial value)
   currentPoint <- initial.value
   randomWalk <- initial.value
+  vec <- vector()
+  #for loop which starts at 2 because our stating point is already initialised
+  #iterate through until we complete the desired number of steps
   for(i in 2:numSteps){
+    #The current position of the walk is the position of the previous point + a 
+    #random value taken from a uniform distribution where the range varies from -stepRange to + stepRange.
     randomWalk[i] <- randomWalk[i - 1] + runif(1, -stepRange, stepRange)
-    if(i == 0){
-      print(initial.value)
+    if(i == 2){
+      #print(initial.value) #The first value always printed is the initial position (when i is equal to 2 since
+      vec <- c(vec, initial.value)
+      # thats where the for loop starts)
     }else{
       #print(currentPoint + c(0, cumsum(randomWalk[i])))
-      print(randomWalk[i])
+      #print(randomWalk[i])
+      vec <- c(vec, randomWalk[i])
     }
     currentPoint <- randomWalk[i]
   }
+  print(vec)
   plot(randomWalk, type = "l", main = paste("Random Walks Generated From ", numSteps, " Steps"))
 }
-
+#Calling the above function with a 100 steps each with a maximum size of 1 from each current position
 generateWalkFunc(100, 1)
-
-
-#EXAMPLE OF STACKOVERFLOW
-GenerateRandomWalk <- function(k = 250,initial.value = 0) {
-  # Add a bionomial at each step
-  samples = rbinom(k,1,0.5)
-  samples[samples==0] = -1
-  initial.value + c(0, cumsum(samples))
-}
-GenerateRandomWalk(10,0)
